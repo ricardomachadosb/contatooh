7c36e7812e130227ffb3243e826e2698e2b5f088
Ricardo Machado <ricardomachado.sb@gmail.com>
2015-04-19 19:32:22 -0300

    chapter 3

--- config/express.js
+++ config/express.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 module.exports = function(){
   var app = express();
 
   app.set('port', 3000);
 
+  //middleware
+  app.use(express.static('./public'));
   app.set('view engine', 'ejs');
   app.set('views', './app/views');
 
-  //middleware
-  app.use(express.static('./public'));
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')());
   //app.use(app.router);
 
   load('models', {cwd: 'app'})
